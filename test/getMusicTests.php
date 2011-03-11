b2b326fea57a166d7d87c8b15031bb7ebeea6bb7
puzanov <puzanov@gmail.com>
2011-03-11 15:54:06 +0600

    1st red line mofo

--- test/getMusicTests.php
+++ test/getMusicTests.php
@@ -3,7 +3,14 @@ include "test/config.php";
 
 class HolderTests extends PHPUnit_Framework_TestCase { 
   public function testGetCover() {
-  
+    $builder = new PlaylistBuilder();
+    $playlist = $builder->build("/path/to/playlist");
+    $this->assertEquals("Playlist", get_class($playlist));
   }
 }
 
+class PlaylistBuilder {
+  public function build($pathToPlaylistFiles) {
+  
+  }
+}
