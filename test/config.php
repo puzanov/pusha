97d604e8bdc4f7895e2cdb982fdc10d2763e7c1a
puzanov <puzanov@gmail.com>
2011-03-11 15:40:14 +0600

    first test and dummy music folder

---
+++ test/config.php
@@ -0,0 +1,4 @@
+<?
+error_reporting(E_ALL^E_NOTICE);
+require_once 'PHPUnit/Framework.php';
+
---
+++ test/getMusicTests.php
@@ -0,0 +1,9 @@
+<?php
+include "test/config.php";
+
+class HolderTests extends PHPUnit_Framework_TestCase { 
+  public function testGetCover() {
+  
+  }
+}
+
