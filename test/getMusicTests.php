a24abec5098f4eaf4824143cb4e6826dc7234b8f
puzanov <puzanov@gmail.com>
2011-03-11 16:14:03 +0600

    test for playlist cover

--- test/getMusicTests.php
+++ test/getMusicTests.php
@@ -2,19 +2,25 @@
 include "test/config.php";
 
 class HolderTests extends PHPUnit_Framework_TestCase { 
-  public function testGetCover() {
+  public function testGetValid() {
     $builder = new PlaylistBuilder();
-    $playlist = $builder->build("/path/to/playlist");
+    $playlist = $builder->build(__DIR__ . "/music/Metallica - Load - 1997");
     $this->assertEquals("Playlist", get_class($playlist));
+    $this->assertEquals("/mnt/src/pusha/test/music/Metallica - Load - 1997/cover.jpg", $playlist->cover);
   }
 }
 
 class PlaylistBuilder {
   public function build($pathToPlaylistFiles) {
-    return new Playlist(); 
+    $playlist = new Playlist();
+    $playlist->cover = $pathToPlaylistFiles . "/cover.jpg";
+    if (!is_file($playlist->cover)) {
+      // raise ex 
+    }
+    return $playlist;
   }
 }
 
 class Playlist {
-
+  var $cover;
 }
