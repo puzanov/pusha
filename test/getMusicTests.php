fbe3f775199250f56808bc77362d1f05081ec37f
puzanov <puzanov@gmail.com>
2011-03-11 16:48:06 +0600

    track tests added

--- test/getMusicTests.php
+++ test/getMusicTests.php
@@ -6,9 +6,12 @@ class HolderTests extends PHPUnit_Framework_TestCase {
     $builder = new PlaylistBuilder();
     $playlist = $builder->build(__DIR__ . "/music/Metallica - Load - 1997");
     $this->assertEquals("Playlist", get_class($playlist));
-    $this->assertEquals("/mnt/src/pusha/test/music/Metallica - Load - 1997/cover.jpg", $playlist->cover);
+    $this->assertEquals(__DIR__ . "/music/Metallica - Load - 1997/cover.jpg", $playlist->cover);
     $this->assertEquals("Metallica - Load - 1997", $playlist->title);
     $this->assertEquals("Metallica Load Remastered", trim($playlist->info));
+    $this->assertEquals(__DIR__ . "/music/Metallica - Load - 1997/track1.mp3", $playlist->tracks[0]);
+    $this->assertEquals(__DIR__ . "/music/Metallica - Load - 1997/track2.mp3", $playlist->tracks[1]);
+    $this->assertEquals(__DIR__ . "/music/Metallica - Load - 1997/track3.mp3", $playlist->tracks[2]);
   }
 }
 
@@ -21,7 +24,7 @@ class PlaylistBuilder {
     }
     $playlist->title = array_pop(explode("/", $pathToPlaylistFiles));
     $playlist->info = file_get_contents($pathToPlaylistFiles . "/info.txt");
-
+    $playlist->tracks = glob("{$pathToPlaylistFiles}/*.mp3");
     return $playlist;
   }
 }
