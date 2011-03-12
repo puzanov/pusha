d5ea127345b99d89a63dd1e0f81e6f14201ab37f
puzanov <puzanov@gmail.com>
2011-03-12 20:21:32 +0600

    added test for playlist creation

--- test/playlistCreatorTests.php
+++ test/playlistCreatorTests.php
@@ -2,7 +2,18 @@
 include "test/config.php";
 
 class PlaylistCreatorTests extends PHPUnit_Framework_TestCase { 
-  public function testCreatePlaylist() {
-      
+  public function testCreatePlaylistNoCover() {
+    $config = yaml_parse(file_get_contents("config.yml"));      
+    require_once 'HTTP/Request2.php';
+    $request = new HTTP_Request2($config["create_playlist_url"]);
+    $request->setMethod(HTTP_Request2::METHOD_POST)
+        ->addPostParameter('username', $config["username"])
+        ->addPostParameter('password', $config["password"])
+        ->addPostParameter('playlist_name', "name")
+        ->addPostParameter('playlist_text', "text");
+        #->addUpload('avatar', './exploit.exe', 'me_and_my_cat.jpg', 'image/jpeg');
+    $json = $request->send()->getBody();    
+    $result = json_decode($json);
+    $this->assertEquals("OK", $result->status);
   }
 }
