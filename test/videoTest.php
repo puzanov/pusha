083c4657c9dfa339f29b94aebbd5a0c3a3353465
puzanov <puzanov@gmail.com>
2011-03-17 18:10:14 +0600

    video uploader test added

--- test/videoTest.php
+++ test/videoTest.php
@@ -1,18 +1,46 @@
 <? 
 include "config.php";
 require_once 'HTTP/Request2.php';
+require_once "lib/HttpClient.php";
 
 class VideoTests extends PHPUnit_Framework_TestCase {
   public function testGet() {
     $config = yaml_parse(file_get_contents("config.yml"));    
-    $request = new HTTP_Request2("http://login.namba.dev/login.php");
-    $request->setConfig(array("follow_redirects" => true));
-    $request->setMethod(HTTP_Request2::METHOD_POST)
-                  ->addPostParameter('login', $config["username"])
-                  ->addPostParameter('password', $config["password"])
-                  ->addPostParameter('sub', 'Войти');
-    $result = $request->send()->getBody();  
-    echo $result;
+    $client = new HttpClient('login.namba.test');
+    $client->cookie_host = 'namba.test';
+    $client->handle_redirects;
+    $client->referer = 'http://video.namba.test/add.php';
+    $client->post('/login.php', array(
+        'login' => $config['username'],
+        'password' => $config['password'],
+        'sub' => 'Войти'
+    ));
+    
+    $content = $client->getContent();
+    $resForSession = array();
+    preg_match('/\<input name=\"ses\" type=\"hidden\" value=\"(.*)\"\/\>/', $content, $resForSession);
+    $ses = $resForSession[1];
+    $resForUploadhost = array();
+    preg_match("/var uploadHost \= '(.*)'\;/", $content, $resForUploadhost);
+    $uploadHost = $resForUploadhost[1];
+
+    var_dump($ses);
+    var_dump($uploadHost);
+
+    $uploader = new CurlFileUploader(
+        '/mnt/src/pusha/test/video/hack.flv',
+        'http://'.$uploadHost.'/upload',
+        'file',
+        array(
+            'ses'=>$ses,
+            'l'=>$config['username'],
+            'video_service'=>1,
+            'title'=>'test video',
+            'category_id'=>1
+        )
+    );
+
+    $uploader->UploadFile();
   }
 }
 
@@ -40,11 +68,9 @@ class NambaVideoKGUploader {
     $resForSession = array();
     preg_match('/\<input name=\"ses\" type=\"hidden\" value=\"(.*)\"\/\>/', $content, $resForSession);
     $ses = $resForSession[1];
-    $this->logger->info('ses='.$ses);
     $resForUploadhost = array();
     preg_match("/var uploadHost \= '(.*)'\;/", $content, $resForUploadhost);
     $uploadHost = $resForUploadhost[1];
-    $this->logger->info('uploadhost='.$uploadHost);
 
     $title = explode('/', $filepath);
 
@@ -107,7 +133,6 @@ class CurlFileUploader {
     curl_setopt($ch, CURLOPT_POST, 1 );
     curl_setopt($ch, CURLOPT_HEADER, 1 );
     curl_setopt($ch, CURLOPT_HTTPHEADER, array('X-Real-IP: '.$_SERVER['REMOTE_ADDR']));
-    var_dump($this->postParams);
     curl_setopt($ch, CURLOPT_POSTFIELDS, $this->postParams);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     #curl_setopt($ch, CURLOPT_, 1);
