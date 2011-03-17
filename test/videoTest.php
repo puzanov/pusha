bc151ccb64a754b501633225b4e42fcdb5c58ff3
puzanov <puzanov@gmail.com>
2011-03-17 20:17:08 +0600

    video tests are done

--- test/videoTest.php
+++ test/videoTest.php
@@ -5,17 +5,43 @@ require_once "lib/HttpClient.php";
 require_once "lib/CurlFileUploader.php";
 
 class VideoTests extends PHPUnit_Framework_TestCase {
-  public function testGet() {
+  public function testUploadVideo() {
     $video_uploader = new VideoUploader();
     $video_uploader->upload('/mnt/src/pusha/test/video/osel.flv', 1);
+  }
+
+  public function testGetTitle() {
+    $video_uploader = new VideoUploader();
+    $title = $video_uploader->getTitle('/mnt/src/pusha/test/video/osel.flv');
+    $this->assertEquals("osel", $title);
+    $title = $video_uploader->getTitle('/mnt/src/pusha/test/video/pidaraz.osel.flv');
+    $this->assertEquals("pidaraz.osel", $title);
+    $title = $video_uploader->getTitle('/mnt/src/pusha/test/video/Терминатор 4 - полная версия.HD720p.DVDRIP.mpeg');
+    $this->assertEquals("Терминатор 4 - полная версия.HD720p.DVDRIP", $title);
+    $title = $video_uploader->getTitle('/mnt/src/pusha/test/video/Файл без расширения');
+    $this->assertEquals("Файл без расширения", $title);
   }
 }
 
 class VideoUploader {
   var $config = array();
+  
   public function __construct() {
     $this->config = yaml_parse(file_get_contents("config.yml"));    
   }
+  
+  public function getTitle($video_path) {
+    $dir_chunks = explode("/", $video_path);
+    $filename = $dir_chunks[count($dir_chunks)-1];
+    $dotted_title = explode(".", $filename);
+    if (count($dotted_title) == 2) return $dotted_title[0];
+    if (count($dotted_title) > 2) {
+      array_pop($dotted_title);
+      return implode(".", $dotted_title);
+    }
+    return $dotted_title[0];
+  }
+  
   public function upload($video_path, $category_id) {
     $client = new HttpClient($this->config['login_host']);
     $client->handle_redirects;
@@ -44,11 +70,11 @@ class VideoUploader {
       'http://'.$uploadHost.'/upload',
       'file',
       array(
-          'ses'=>$ses,
-          'l'=>$this->config['username'],
-          'video_service'=>1,
-          'title'=>'test video',
-          'category_id'=>$category_id
+        'ses'=>$ses,
+        'l'=>$this->config['username'],
+        'video_service'=>1,
+        'title'=>$this->getTitle($video_path),
+        'category_id'=>$category_id
       )
     );
 
