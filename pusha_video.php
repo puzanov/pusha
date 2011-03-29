a12d62c7957a1e97d695c4ea1b62b2d132509de5
puzanov <puzanov@gmail.com>
2011-03-29 11:33:15 +0600

    video is done

--- pusha_video.php
+++ pusha_video.php
@@ -1,8 +1,19 @@
 <? 
+require_once "lib/VideoUploader.php";
+require_once "lib/HttpClient.php";
+require_once "lib/CurlFileUploader.php";
+
 $config = yaml_parse(file_get_contents("config.yml"));
 $d = dir($config["video_path"]);
 while (false !== ($entry = $d->read())) {
   if ($entry == ".." || $entry == ".") continue;
   if (is_file($config['video_path']."/".$entry)) continue; 
-
+  $category_id = $entry;
+  $video_uploader = new VideoUploader();
+  $dv = dir($config['video_path']."/".$entry);
+  while (false !== ($video = $dv->read())) {
+    if ($video == ".." || $video == ".") continue;
+    if (is_dir($config['video_path']."/".$entry."/".$video)) continue;
+    $video_uploader->upload($config['video_path']."/".$entry."/".$video, $category_id);
+  }  
 }
