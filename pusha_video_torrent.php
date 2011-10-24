d0f63f3319784697656cdc1bbfc7cbaab3b53b1a
Oleg Puzanov <puzanov@gmail.com>
2011-10-24 15:45:59 +0600

    torrent in category number 2

--- pusha_video_torrent.php
+++ pusha_video_torrent.php
@@ -1,7 +1,7 @@
 <?
 system("cd torrent");
 system("curl -L --cookie nada --data 'login_username=parapapa&login_password=qwerty&login=Вход' http://torrent.kg/forum/login.php?redirect=/forum/download.php?id={$argv[1]} > 1.torrent");
-system("cd videos/16; ctorrent -e 0 ../../1.torrent");
+system("cd videos/2; ctorrent -e 0 ../../1.torrent");
 system("php pusha_video.php");
-system("rm -rf videos/16/*");
+system("rm -rf videos/2/*");
 ?>
