c772edc9c9c866456e9a08e7a020359efdfdeabb
Oleg Puzanov <puzanov@gmail.com>
2011-11-03 23:46:37 +0600

    videobash 1st version is done

--- pusha_video_videobash.php
+++ pusha_video_videobash.php
@@ -1,12 +1,15 @@
 <?
   // getting video from videobash.com
 
-include "lib/GoogleTranslate.php";
 include "lib/nokogiri.php";
 
-$tr = new Google_Translate_API();
-echo $tr->translate("world", "en", "ru");
-exit;
+$used_file = 'used.txt';
+if (file_exists($used_file) == false) {
+  file_put_contents($used_file, '');
+  $used = array();
+} else {
+  $used = explode("\n", file_get_contents($used_file));
+}
 
 $content = file_get_contents("http://videobash.com");
 $saw = new nokogiri($content);
@@ -20,10 +23,20 @@ foreach ($hrefs as $href) {
   $saw = new nokogiri($video_content);
   $flashvars = $saw->get("param[name=flashvars]")->toArray();
   $title = $saw->get("h1.video-title")->toArray();
-  $title = $title['#text'];
-  var_dump($title);exit;
+  $title = preg_replace("/[^\ 0-9a-zA-Zа-яА-Я]/u", "", $title['#text']);
+  if (isset($used[$title])) {
+    echo "we have this video\n";
+    continue;
+  } else {
+    echo "video is new\n";
+  }
   $qs = $flashvars['value'];
   parse_str($qs, $params);
   $video_url = $params['video_url'];
   var_dump($video_url);
+  var_dump($title);
+  system("wget \"$video_url\" -O \"videos/1/{$title}.flv\"");
 }
+file_put_contents($used_file, implode("\n", $used));
+system("php pusha_video.php");
+system("rm -rf videos/1/*");
