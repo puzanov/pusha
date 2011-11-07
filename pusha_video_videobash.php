215609943115219929ba10bae0d94fa65091fa80
Oleg Puzanov <puzanov@gmail.com>
2011-11-07 10:07:12 +0600

    videobash is done

--- pusha_video_videobash.php
+++ pusha_video_videobash.php
@@ -1,7 +1,11 @@
 <?
-  // getting video from videobash.com
+// getting video from videobash.com
 
+error_reporting(E_ALL ^ E_NOTICE);
+include "lib/gootrans.php";
 include "lib/nokogiri.php";
+
+$gootrans = new gootrans();
 
 $used_file = 'used.txt';
 if (file_exists($used_file) == false) {
@@ -11,32 +15,39 @@ if (file_exists($used_file) == false) {
   $used = explode("\n", file_get_contents($used_file));
 }
 
-$content = file_get_contents("http://videobash.com");
-$saw = new nokogiri($content);
-$hrefs = $saw->get("#topVideo li div div a")->toArray();
-foreach ($hrefs as $href) {
-  if (empty($href['img'])) {
-    continue;
+while (true) {
+  $content = file_get_contents("http://videobash.com");
+  $saw     = new nokogiri($content);
+  $hrefs   = $saw->get("#topVideo li div div a")->toArray();
+
+  foreach ($hrefs as $href) {
+    if (empty($href['img'])) {
+      continue;
+    }
+
+    $url           = $href['href'];
+    $video_content = file_get_contents($url);
+    $saw           = new nokogiri($video_content);
+    $flashvars     = $saw->get("param[name=flashvars]")->toArray();
+    
+    $title     = $saw->get("h1.video-title")->toArray();
+    $title     = preg_replace("/[^\ 0-9a-zA-Zа-яА-Я]/u", "", $title['#text']);
+    $title     = $gootrans->translate($title, "en", "ru");
+
+    if (in_array($title, $used)) {
+      echo "$title -- we have this video\n";
+      continue; // going to fetch another video
+    } else {
+      echo "video is new\n";
+    }
+
+    parse_str($flashvars['value'], $params);
+    system("wget \"{$params['video_url']}\" -O \"videos/1/{$title}.flv\"");
+    
+    system("php pusha_video.php");
+    $used[] = $title;
+    file_put_contents($used_file, implode("\n", $used));
+    system("rm -rf videos/1/*");
   }
-  $url = $href['href'];
-  $video_content = file_get_contents($url);
-  $saw = new nokogiri($video_content);
-  $flashvars = $saw->get("param[name=flashvars]")->toArray();
-  $title = $saw->get("h1.video-title")->toArray();
-  $title = preg_replace("/[^\ 0-9a-zA-Zа-яА-Я]/u", "", $title['#text']);
-  if (isset($used[$title])) {
-    echo "we have this video\n";
-    continue;
-  } else {
-    echo "video is new\n";
-  }
-  $qs = $flashvars['value'];
-  parse_str($qs, $params);
-  $video_url = $params['video_url'];
-  var_dump($video_url);
-  var_dump($title);
-  system("wget \"$video_url\" -O \"videos/1/{$title}.flv\"");
+  sleep(600);
 }
-file_put_contents($used_file, implode("\n", $used));
-system("php pusha_video.php");
-system("rm -rf videos/1/*");
